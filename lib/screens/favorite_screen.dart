b431355c3c509f62a672724d0dcd36064d04dacb
Mohsen <83723914+mohsenrahmdeli@users.noreply.github.com>
2024-10-02 11:59:57 +0330

    Add files via upload

---
+++ lib/screens/favorite_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+import 'home_screen.dart';
+
+class FavoriteScreen extends StatelessWidget {
+  const FavoriteScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple.shade800.withOpacity(0.8),
+        elevation: 0,
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
+          onPressed: () => Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const HomeScreen(),
+            ),
+          ),
+        ),
+      ),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Colors.deepPurple.shade800.withOpacity(0.8),
+              Colors.deepPurple.shade200.withOpacity(0.8),
+            ],
+          ),
+        ),
+        child: const Center(
+          child: Text('Favorite Page'),
+        ),
+      ),
+    );
+  }
+}
